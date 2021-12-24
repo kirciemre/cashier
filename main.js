5f3d3e5483c2c9c118cdeb69be0d7c7e55011c68
Emre KIRCI <emre@emrekirci.com>
2021-12-24 23:36:56 +0300

    Update main.js
    finished

--- main.js
+++ main.js
@@ -1,67 +1,56 @@
 function checkCashRegister(price, cash, cid) {
-  let change = 0;
-  let status = "OPEN";
-  const currency = [
-    ["PENNY" , 0.01],
-    ["NICKEL" , 0.05],
-    ["DIME" , 0.1],
-    ["QUARTER", 0.25],
-    ["ONE" , 1],
-    ["FIVE", 5],
-    ["TEN" , 10],
-    ["TWENTY", 20],
-    ["ONE HUNDRED" ,100]
-  ].reverse();
-  change = cash-price;
-  let cashInDrawer = [...cid]
-  let allCash = cashInDrawer.reduce(function(acc, val) { return acc + val[1]; }, 0).toFixed(2);
-  if (change > allCash){
-    status = "INSUFFICIENT_FUNDS";
-    change = giveChange(change);
-  }else if (change == allCash){
-    status = "CLOSED"
-    change = giveChange(change);
-  }else {
-    status = "OPEN"
-    change = giveChange(change);
-  }
+    let status = "OPEN" //status default OPEN
+    let output = { }; //output format initialized
+    const currencyVals = [ //currency without float so don't need toFixed(2)
+        ["ONE HUNDRED", 10000],
+        ["TWENTY", 2000],
+        ["TEN", 1000],
+        ["FIVE", 500],
+        ["ONE", 100],
+        ["QUARTER", 25],
+        ["DIME", 10],
+        ["NICKEL", 5],
+        ["PENNY", 1]
+    ];
+    let changeBasket =[]; //initialize changeBasket
 
-  function giveChange(change){
-    for(let i = 0; i < currency.length; i++){
-      if(change/currency[i][1] >= 1 ){
-        var money = currency[i][0];
-        return paybackIsEnought(i,change)
-      }
+    var fullCash = cid.reduce((accu, item) => { //overall amout of $ in drawer
+        accu[item[0]] = Math.round(item[1] * 100);
+        accu.total += accu[item[0]];
+        return accu;
+    }, { total: 0 }); //calculate drawers cash storage with default value 0
+         
+    var change = Math.round((cash - price) * 100); //change convert to cent
+      
+    if (change == fullCash.total) { //easiest condition first!
+        status = 'CLOSED';
+        changeBasket = cid;
     }
-  }
-
-  function paybackIsEnought(index, changeVal) {
-          
-    let ustu = [];
-    
-    if(changeVal===cid.reverse()[index][1]){
-      status = "CLOSED";
-      ustu.push([cid.reverse()[index]]);
+         
+    else if (change > fullCash.total) { //2nd easiest condition!
+        status = 'INSUFFICIENT_FUNDS';
     }
-    else{
-      for(let j=index; j<cid.length; j++){
-        if(changeVal<cid.reverse()[j][1]){
-          console.log("changeVal");
-          ustu.push([cid.reverse()[j][0], changeVal])
-          
-          break;
-        }else{
-          ustu.push(cid.reverse()[j][0], cid.reverse()[j][1]);
-          changeVal = changeVal- cid.reverse()[index][1];
+    else
+    { //the last one is complicted a bit!
+        for (let item of currencyVals) {
+          var value = 0;
+          while (fullCash[item[0]] > 0 && change >= item[1]) {
+              change -= item[1];
+              fullCash[item[0]] -= item[1];
+              value += item[1];
+          }
+          if (value > 0) {
+              changeBasket.push([item[0], value / 100]);
+          }
         }
-      }
+
+        if (change > 0) { // (change < fullCash.total) but there is not      enought small coin to payback! so change cant'be 0. so there is again insufficient fund! don't forget to empty basket!
+        status = 'INSUFFICIENT_FUNDS';
+        changeBasket = [];
+        }
     }
-    return ustu;
-
-   
-  }
-
-  return {"status": status, "change": change};
+    output = { "status": status, change: changeBasket}; //output format finalized!
+    return output; //it's done gg!
 }
 
-console.log(checkCashRegister(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]));
+console.log(checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 1], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]));
